1bfdf5457b5ca7bf8b9dc6803685eacab1d6ebdf
abdurahmon97v <abdurahmon97v@gmail.com>
2023-06-14 21:37:13 +0500

    .

--- src/pages/auth/components/register-form/register-form.jsx
+++ src/pages/auth/components/register-form/register-form.jsx
@@ -11,7 +11,7 @@ export const RegisterForm = () => {
 
   React.useEffect(() => {
     if (loadState("token")) {
-      navigate("/main");
+      navigate("/main", { replace: true });
     }
   }, []);
 
